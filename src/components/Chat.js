407b0915b06bbcd08eeece3f56208729a2aeebe7
sahas-arora <sahasarora@gmail.com>
2021-03-01 18:13:25 +0100

    Chat header, body and footer added

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -2,15 +2,23 @@ import React from "react";
 import { Avatar, IconButton } from "@material-ui/core";
 import { SearchOutlined, AttachFile, MoreVert} from '@material-ui/icons';
 
+import { SelectedSeed } from "../hooks/selectedSeed";
+
 
 import "../styles/Chat.css";
 
-const Chat = ({ avatarSource }) => {
+const Chat = () => {
+
+    const [selectedSeed, setSeed] = SelectedSeed();
+
+    console.log("Chat yo ", selectedSeed);
+    const url = `https://avatars.dicebear.com/api/human/${selectedSeed}.svg`
+
     return(
         <div className="chats">
             
             <div className="chats-header">
-                    <Avatar />
+                    <Avatar src={url} />
                     {/* <Avatar src={avatarSource} /> */}
 
                 <div className="chats-header-info">
@@ -29,6 +37,10 @@ const Chat = ({ avatarSource }) => {
                         <MoreVert />
                     </IconButton>
                 </div>
+            </div>
+
+            <div className="chats-message">
+                Here!!
             </div>
 
             <div className="chats-input-panel">
